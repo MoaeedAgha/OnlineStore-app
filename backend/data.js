3c31eaebeef47dc1b54785bca723b35dfbf2ce17
Moaeed Agha <moaeed.agha@gmail.com>
2022-05-19 16:45:26 +0500

    Created Node App, Installed express and basic server file

---
+++ backend/data.js
@@ -0,0 +1,78 @@
+const data = {
+    products:[
+        {
+            _id: '1',
+            name: 'Nike Shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 10,
+            description: 'High quality product'
+        },
+        {
+            _id: '2',
+            name: 'Addidas Shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 100,
+            brand: 'Addidas',
+            rating: 4,
+            numReviews: 15,
+            countInStock: 20,
+            description: 'High quality product'
+        },
+        {
+            _id: '3',
+            name: 'Nike',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 110,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 21,
+            countInStock: 30,
+            description: 'High quality product'
+        },
+        {
+            _id: '4',
+            name: 'Nike',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 130,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 10,
+            description: 'High quality product'
+        },
+        {
+            _id: '5',
+            name: 'Nike',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 140,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 10,
+            description: 'High quality product'
+        },
+        {
+            _id: '6',
+            name: 'Nike',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 150,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 10,
+            description: 'High quality product'
+        },
+    ]
+};
+
+export default data;
