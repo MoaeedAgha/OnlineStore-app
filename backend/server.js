e47a4eab558c08b5dff84367012f4de14ad6fd96
Moaeed Agha <moaeed.agha@gmail.com>
2022-06-07 17:13:02 +0500

    Implementation of SignIn, SignOut action,reducer,screens

--- backend/server.js
+++ backend/server.js
@@ -8,21 +8,10 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-mongoose.connect('mongodbUrl' , {
+mongoose.connect('mongodb' , {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
-// app.get('/api/products' , (req , res) => {
-//     res.send(data.products);
-// })
-// app.get('/api/products/:id' , (req , res) =>{
-//     const product = data.products.find((x) => x._id === req.params.id);
-//     if(product){
-//         res.send(product);
-//     }else{
-//         res.status(404).send({ message: 'Product not found'});
-//     }
-// });
 
 app.use('/api/users' , userRouter);
 app.use('/api/products' , productRouter);
