7351bf3c1fb5ad8ca89b87185a9408176dec53a2
Moaeed Agha <moaeed.agha@gmail.com>
2022-06-11 03:08:27 +0500

    Place Order screen with calculations

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import RegisterScreen from './screens/RegisterScreen';
 import SigninScreen from './screens/SigninScreen';
 import ShippingAddressScreen from './screens/ShippingAddressScreen';
 import PaymentMethodScreen from './screens/PaymentMethodScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
 function App() {
   const cart = useSelector((state) => state.cart);
   const {cartItems} = cart;
@@ -58,6 +59,7 @@ function App() {
                 <Route path="/register" element={<RegisterScreen />}></Route>
                 <Route path="/shipping" element={<ShippingAddressScreen />}></Route>
                 <Route path="/payment" element={<PaymentMethodScreen />}></Route>
+                <Route path="/placeorder" element={<PlaceOrderScreen />}></Route>
                 <Route path='/' element={< HomeScreen />} exact></Route>
               </Routes>
             </main>
